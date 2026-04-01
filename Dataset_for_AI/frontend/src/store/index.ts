7e3abe95086ca766a74d4e27836ff2082c214711
ArturSuntsov <ais1166611@gmail.com>
2026-04-01 22:16:55 +0500

    Delete Dataset_for_AI directory

--- Dataset_for_AI/frontend/src/store/index.ts
+++
@@ -1,5 +0,0 @@
-export { useAuthStore } from "./useAuthStore";
-export { useDatasetStore } from "./useDatasetStore";
-export { useTaskStore } from "./useTaskStore";
-export { useFinanceStore } from "./useFinanceStore";
-
